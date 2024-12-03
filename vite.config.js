72b7c856fb3442b8dd4586c13e65c82b48fd205b
Edward Ibarra <edwardibarra@Edwards-MacBook-Air.local>
2024-12-02 22:48:50 -0500

    ghpages changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+imimport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ie4520unessay/',  // Replace with your repository name
 })
